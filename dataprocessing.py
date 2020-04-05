further improvements, bug fixing, normalization

dataprocessing.py
@@ -60,7 +60,7 @@ def save_partition(n_partition, accstr, accnum, output):
     lg.debug('saved: ' + output + 'array_' + str(n_partition) + '.npy')
 
 
-def parse_moments(momentfile, rescale=True):
+def parse_moments(momentfile, rescale=False):
     momentsel = 4
 
     moments = pd.read_csv(momentfile, index_col=0, usecols=[0, momentsel], parse_dates=True).dropna()
@@ -90,12 +90,21 @@ class MultiSet(Dataset):
         self.embedlen = 24
         self.analyser = SentimentIntensityAnalyzer()
         # arrays with id's of preembedded picturees
+        self.norms = np.ones(24)
+
         self.init_imagefolder(preembedfolder)
+
         self.date_arr, self.datedict = read_json_aapl(jsonfile, self.contig_ids)
 
         self.response_dates, self.response, self.scale = parse_moments(self.prefs['moments'])
-
+        self.training_idx, self.test_idx = [], []
         lg.info("finished initializing the dataset")
+
+    def create_valsplit(self, distr=0.8):
+        l = self.date_arr.shape[0]
+        n_train = int(l * distr)
+        indices = np.random.permutation(l)
+        self.training_idx, self.test_idx = indices[:n_train], indices[n_train:]
 
     def init_imagefolder(self, preembedfolder, contig=True):
         lg.info("starting preembed data processing")
@@ -140,10 +149,10 @@ class MultiSet(Dataset):
         inds = np.argsort(interm_contig_ids)
         interm_contig_ids = interm_contig_ids[inds]
         interm_contig_vals = interm_contig_vals[inds]
-
+        self.norms = np.linalg.norm(interm_contig_vals, axis=0)
+        interm_contig_vals = interm_contig_vals / self.norms
         self.contig_ids = np.ascontiguousarray(interm_contig_ids)
         self.contig_vals = np.ascontiguousarray(interm_contig_vals)
-
         lg.info("reallocation done ")
 
         # for u in unsorted:
@@ -195,7 +204,7 @@ class MultiSet(Dataset):
         return s
 
     def __getitem__(self, index):
-        # lg.debug("getting item: %s",index)
+        lg.debug("getting item: %s", index)
         date = self.date_arr[index]
         # lg.debug(date)
         sample = self.datedict[date]
@@ -203,12 +212,12 @@ class MultiSet(Dataset):
         j = 0
         for (img_id, content) in sample:
             sent = self.analyser.polarity_scores(content)
-            arr = np.empty(self.embedlen + 4)
+            arr = np.zeros(self.embedlen + 4)
             arr[:4] = [sent['neg'], sent['neu'], sent['pos'], sent['compound']]
 
             ind = self.contig_ids.searchsorted(img_id)
             if ind < self.contig_ids.shape[0] and self.contig_ids[ind] == img_id:
-                arr[4:] = self.contig_ids[index]
+                arr[4:] = self.contig_vals[index]
                 data[j] = arr
                 j += 1
             else:
@@ -230,7 +239,6 @@ class MultiSampler(torch.utils.data.Sampler):
 
     def __init__(self, data_source):
         super().__init__(data_source)
-        self.l = None
         self.data_source = data_source
         arr = self.data_source.date_arr
         lg.debug("sampler got %s dates inputside", len(arr))
@@ -239,20 +247,27 @@ class MultiSampler(torch.utils.data.Sampler):
         lg.info("sampler got %s dates responseside", len(rarr))
 
         _, self.inds, _ = np.intersect1d(arr, rarr, return_indices=True)
-
+        self.l = len(self.inds)
         lg.info("sampler inititiated with %s samples", len(self))
 
     def __iter__(self):
         return iter(np.random.permutation(self.inds))
 
     def __len__(self):
-        if self.l is None:
-            arr = self.data_source.date_arr
-            rarr = self.data_source.response_dates
-            _, inds, _ = np.intersect1d(arr, rarr, return_indices=True)
-            self.l = len(inds)
-
         return self.l
+
+
+class MultiSplitSampler(MultiSampler):
+
+    def __init__(self, data_source, train=True):
+        super().__init__(data_source)
+        inds = self.data_source.training_idx if train else self.data_source.test_idx
+        self.inds = np.intersect1d(self.inds, inds)
+        self.l = len(self.inds)
+        lg.info("split sampler inititiated with %s samples", len(self))
+
+    def __iter__(self):
+        return iter(np.random.permutation(self.inds))
 
 
 def read_json_aapl(jsonfile, contig_ids):

embed_nets.py
@@ -1,3 +1,5 @@
+import logging as lg
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,13 +13,11 @@ class Pre_Net(nn.Module):
         self.c1 = nn.Conv1d(28, 10, 1)
         self.avg_pool = torch.nn.AdaptiveAvgPool1d(1)
         self.max_pool = torch.nn.AdaptiveMaxPool1d(1)
-
         self.linear = nn.Linear(76, 10)
         self.out = nn.Linear(10, 1)
 
     def forward(self, x):
         xc = self.c1(x)
-
         x = torch.cat((xc, x), 1)
         x = F.relu(x)
         x1 = self.avg_pool(x)
@@ -37,11 +37,63 @@ class Pre_Net(nn.Module):
     #     return num_features
 
 
-
 class Net_Container:
-    def __init__(self, net, dataloader, optimizer, criterion, ):
+    def __init__(self, net, dataloader, optimizer, criterion, validation=False, val_loader=None):
         self.dataloader = dataloader
         self.optimizer = optimizer
         self.criterion = criterion
         self.optimizer.zero_grad()
         self.net = net
+        self.validation = validation
+        assert (not validation) or val_loader is not None
+        self.val_loader = val_loader
+
+    def train(self, epochs):
+        net = self.net
+        dataloader = self.dataloader
+        optimizer = self.optimizer
+        criterion = self.criterion
+
+        for epoch in range(epochs):
+            epoch_len = 0
+            running_loss = 0.0
+            total_loss = 0.
+            net.train()
+            optimizer.zero_grad()
+            for i, data in enumerate(dataloader, 0):
+                inputs, resp = data[0].double(), data[1].double()
+                outputs = net(inputs)
+                loss = criterion(outputs.squeeze(), resp.squeeze())
+                running_loss += loss.item()
+                if (i + 1) % 20 == 0:
+                    loss = total_loss * 0.9 + loss * 0.1
+                    loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    total_loss = 0.0
+                else:
+                    total_loss += loss
+                epoch_len += 1
+            lg.info('e: %d | %s training_loss: %.10f', epoch + 1, epoch_len, (running_loss / epoch_len))
+            optimizer.zero_grad()
+            if not self.validation:
+                continue
+            net.eval()
+            val_loss = 0.0
+            val_size = 0
+            min_out = 5.0
+            max_out = 0.0
+            with torch.no_grad():
+                for i, data in enumerate(self.val_loader, 0):
+                    inputs, resp = data[0].double(), data[1].double()
+                    outputs = net(inputs)
+                    # out=outputs.squeeze().item()
+                    # if out < min_out:
+                    #     min_out=out
+                    # elif out > max_out:
+                    #     max_out = out
+                    loss = criterion(outputs.squeeze(), resp.squeeze())
+                    val_loss += loss.item()
+                    val_size += 1
+            # lg.info('min: %s, max: %s',min_out,max_out)
+            lg.info('e: %d | %s val_loss: %.10f', epoch + 1, val_size, (val_loss / val_size))

main_train.py
@@ -6,7 +6,6 @@ import torch.optim as optim
 import yaml
 from torch.utils.data import Sampler
 
-import backtest
 import dataprocessing
 import embed_nets
 
@@ -14,7 +13,7 @@ logFormatter = lg.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.
 rootLogger = lg.getLogger()
 rootLogger.setLevel(lg.INFO)
 
-fileHandler = lg.FileHandler("train_test_2.log")
+fileHandler = lg.FileHandler("train_test_val2.log")
 fileHandler.setFormatter(logFormatter)
 rootLogger.addHandler(fileHandler)
 
@@ -27,12 +26,18 @@ net.train()
 with open('resources/preferences.yaml') as f:
     prefs = yaml.load(f, Loader=yaml.FullLoader)
 trainset = dataprocessing.MultiSet(prefs)
-tsampler = dataprocessing.MultiSampler(trainset)
+trainset.create_valsplit(0.75)
+tsampler = dataprocessing.MultiSplitSampler(trainset)
+vsampler = dataprocessing.MultiSplitSampler(trainset, False)
+# tsampler = dataprocessing.MultiSampler(trainset)
+# vsampler = dataprocessing.MultiSampler(trainset)
+
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=1, num_workers=4, sampler=tsampler)
+valloader = torch.utils.data.DataLoader(trainset, batch_size=1, num_workers=4, sampler=vsampler)
 
 criterion = nn.MSELoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001)
 
-cont = embed_nets.Net_Container(net, trainloader, optimizer, criterion)
+cont = embed_nets.Net_Container(net, trainloader, optimizer, criterion, True, valloader)
 
-backtest.train(cont, 100)
+cont.train(100)