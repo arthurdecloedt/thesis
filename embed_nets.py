c68cce4c31834d9ddba566c8716a7f151a85a4b3
arthurdecloedt <arthurdecloedt@hotmail.com>
2020-04-10 19:29:55 +0200

    training multiple models simultaneously

--- embed_nets.py
+++ embed_nets.py
@@ -9,7 +9,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 class Mixed_Net(nn.Module):
-    def __init__(self):
+    def __init__(self, name="mixed_net"):
         super(Mixed_Net, self).__init__()
 
         self.c1 = nn.Conv1d(28, 10, 1)
@@ -17,6 +17,7 @@ class Mixed_Net(nn.Module):
         self.max_pool = torch.nn.AdaptiveMaxPool1d(1)
         self.linear = nn.Linear(76, 10)
         self.out = nn.Linear(10, 1)
+        self.name = name
 
     def forward(self, x):
         xc = self.c1(x)
@@ -41,13 +42,14 @@ class Mixed_Net(nn.Module):
 
 class Pre_net(nn.Module):
 
-    def __init__(self):
+    def __init__(self, name="pre_net"):
         super().__init__()
         self.c1 = nn.Conv1d(28, 28, 1)
         self.c2 = nn.Conv1d(28, 28, 1)
         self.c3 = nn.Conv1d(28, 28, 1)
         self.c4 = nn.Conv1d(28, 1, 1)
         self.avg_pool = torch.nn.AdaptiveAvgPool1d(1)
+        self.name = name
 
     def forward(self, x):
         xc = self.c1(x)
@@ -61,26 +63,58 @@ class Pre_net(nn.Module):
         xc = F.relu(xc)
         x = torch.add(x, xc)
         x = self.c4(x)
-        x = F.relu(x)
         x = self.avg_pool(x)
         return x
 
 
 class Pooling_Net(nn.Module):
-    def __init__(self):
+    def __init__(self, name="pooling_net"):
         super().__init__()
-        self.c1 = nn.Conv1d(28, 28, 1)
+        self.c1 = nn.Conv1d(56, 10, 1)
 
-        self.pool1 = nn.MaxPool1d(10, 1, 0, 10, ceil_mode=True)
-        self.c2 = nn.Conv1d(28, 28, 1)
-        self.c3 = nn.Conv1d(28, 28, 1)
-        self.c4 = nn.Conv1d(28, 1, 1)
+        self.pool1 = nn.AvgPool1d(11, 1, 0, ceil_mode=True)
+        self.c2 = nn.Conv1d(20, 10, 1)
+        self.c3 = nn.Conv1d(20, 10, 1)
+        self.c4 = nn.Conv1d(10, 1, 1)
         self.avg_pool = torch.nn.AdaptiveAvgPool1d(1)
+        self.name = name
+
+    def forward(self, x):
+        if (x.shape[2] > 5):
+            xp = F.pad(x, (5, 5), 'circular')
+        else:
+            xp = F.pad(x, (5, 5), 'replicate')
+
+        xp = self.pool1(xp)
+        x = torch.cat((x, xp), 1)
+
+        x = F.relu(self.c1(x))
+
+        if (x.shape[2] > 5):
+            xp = F.pad(x, (5, 5), 'circular')
+        else:
+            xp = F.pad(x, (5, 5), 'replicate')
+
+        xp = self.pool1(xp)
+        x = torch.cat((x, xp), 1)
+        x = F.relu(self.c2(x))
+
+        if (x.shape[2] > 5):
+            xp = F.pad(x, (5, 5), 'circular')
+        else:
+            xp = F.pad(x, (5, 5), 'replicate')
+
+        xp = self.pool1(xp)
+        x = torch.cat((x, xp), 1)
+        x = F.relu(self.c3(x))
+
+        x = self.avg_pool(self.c4(x))
+        return x
 
 
 class Pre_Net_Text(nn.Module):
 
-    def __init__(self):
+    def __init__(self, name="pre_net_text"):
         super(Pre_Net_Text, self).__init__()
 
         super().__init__()
@@ -89,6 +123,7 @@ class Pre_Net_Text(nn.Module):
         self.max_pool = torch.nn.AdaptiveMaxPool1d(1)
         self.linear = nn.Linear(28, 10)
         self.out = nn.Linear(10, 1)
+        self.name = name
 
     def forward(self, x):
         x = x[:, :4]
@@ -120,7 +155,6 @@ class Net_Container:
         self.dataloader = dataloader
         self.optimizer = optimizer
         self.criterion = criterion
-        self.optimizer.zero_grad()
         self.net = net
         self.validation = validation
         assert (not validation) or val_loader is not None
@@ -201,3 +235,104 @@ class Net_Container:
                         'zero': (zero_loss / val_size)
                     }, epoch + 1)
                 return
+
+
+class Multi_Net_Container(Net_Container):
+
+    def __init__(self, nets, dataloader, optimizers, criterions, validation=False, val_loader=None, s_writer=None):
+        assert isinstance(nets, (tuple, list))
+        super().__init__(nets, dataloader, optimizers, criterions, validation, val_loader, s_writer)
+        self.n_nets = len(nets)
+        self.max_namesize = 5
+        for net in nets:
+            if len(net.name) > self.max_namesize:
+                self.max_namesize = len(net.name)
+
+    def train(self, epochs, split_loss=True):
+        nets = self.net
+        dataloader = self.dataloader
+        optimizers = self.optimizer
+        criterion = self.criterion
+        median = torch.tensor(self.dataloader.dataset.baselines[1])
+        median_loss = 0.0
+        zero_loss = 0.0
+
+        for epoch in range(epochs):
+
+            epoch_len = []
+            running_loss = []
+            total_loss = []
+            loss = []
+            for i in range(self.n_nets):
+                nets[i].train()
+                epoch_len.append(0)
+                running_loss.append(0.0)
+                total_loss.append(0.0)
+                optimizers[i].zero_grad()
+                loss.append(None)
+            for i, data in enumerate(dataloader, 0):
+                inputs, resp = data[0].double(), data[1].double()
+                for j in range(self.n_nets):
+                    outputs = nets[j](inputs)
+                    loss[j] = criterion(outputs.squeeze(), resp.squeeze())
+                    running_loss[j] += loss[j].item()
+                    if (i + 1) % 10 == 0:
+                        loss[j] = total_loss[j] * 0.9 + loss[j] * 0.1
+                        loss[j].backward()
+                        optimizers[j].step()
+                        optimizers[j].zero_grad()
+                        total_loss[j] = 0.0
+                    else:
+                        total_loss[j] += loss[j]
+                    epoch_len[j] += 1
+            if total_loss[0] != 0.0:
+                for j in range(self.n_nets):
+                    loss[j] = total_loss[j] * 0.9 + loss[j] * 0.1
+                    loss[j].backward()
+                    optimizers[j].step()
+                    optimizers[j].zero_grad()
+            if self.validation:
+                val_loss = []
+                val_size = []
+                for i in range(self.n_nets):
+                    nets[i].eval()
+                    val_loss.append(0.0)
+                    val_size.append(0)
+
+                with torch.no_grad():
+                    for i, data in enumerate(self.val_loader, 0):
+                        inputs, resp = data[0].double(), data[1].double()
+                        for j in range(self.n_nets):
+                            outputs = nets[j](inputs)
+                            # out=outputs.squeeze().item()
+                            # if out < min_out:
+                            #     min_out=out
+                            # elif out > max_out:
+                            #     max_out = out
+                            loss[j] = criterion(outputs.squeeze(), resp.squeeze())
+                            val_loss[j] += loss[j].item()
+                            val_size[j] += 1
+                            if epoch == 0 and j == 0:
+                                loss_median = criterion(median, resp.squeeze())
+                                loss_zero = criterion(torch.tensor(0), resp.squeeze())
+                                median_loss += loss_median.item()
+                                zero_loss += loss_zero.item()
+                # lg.info('min: %s, max: %s',min_out,max_out)
+                # lg.info('e: %d | %s training_loss: %.10f', epoch + 1, val_size, (val_loss[] / val_size))
+                if self.tensorboard:
+                    # self.s_writer.add_scalar("Loss/Val", (val_loss / val_size), epoch + 1)
+
+                    writerdict = {"median": (median_loss / val_size[0]),
+                                  'zero': (zero_loss / val_size[0])
+                                  }
+                    for i in range(self.n_nets):
+                        writerdict[nets[i].name + ": train"] = (running_loss[i] / epoch_len[0])
+                        writerdict[nets[i].name + ": val"] = (val_loss[i] / val_size[0])
+                        lg.info('%s e: %d | %s training_loss: %.10f',
+                                str.ljust(nets[i].name, self.max_namesize, ' ')
+                                , epoch + 1, epoch_len[0], (running_loss[i] / epoch_len[0]))
+
+                        lg.info('%s e: %d | %s val_loss:      %.10f',
+                                str.ljust(nets[i].name, self.max_namesize, ' ')
+                                , epoch + 1, val_size[0], (val_loss[i] / val_size[0]))
+                    self.s_writer.add_scalars("Loss/combined", writerdict, epoch + 1)
--- main_train.py
+++ main_train.py
@@ -1,7 +1,4 @@
 import logging as lg
-import sys
-import traceback
-import warnings
 
 import torch
 import torch.nn as nn
@@ -12,20 +9,19 @@ from torch.utils.tensorboard import SummaryWriter
 import dataprocessing
 import embed_nets
 
-
-def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
-    log = file if hasattr(file, 'write') else sys.stderr
-    traceback.print_stack(file=log)
-    log.write(warnings.formatwarning(message, category, filename, lineno, line))
-
-
-warnings.showwarning = warn_with_traceback
+# def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
+#     log = file if hasattr(file, 'write') else sys.stderr
+#     traceback.print_stack(file=log)
+#     log.write(warnings.formatwarning(message, category, filename, lineno, line))
+#
+#
+# warnings.showwarning = warn_with_traceback
 
 logFormatter = lg.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 rootLogger = lg.getLogger()
 rootLogger.setLevel(lg.INFO)
 
-fileHandler = lg.FileHandler("train_test_val_batched3.log")
+fileHandler = lg.FileHandler("train_test_val_batched4.log")
 fileHandler.setFormatter(logFormatter)
 rootLogger.addHandler(fileHandler)
 
@@ -43,7 +39,8 @@ net.train()
 lg.info("loading dataset")
 tests = dataprocessing.MultiSet.from_file()
 
-tests.create_temporal_valsplit(0.75)
+tests.resp_arr = tests.resp_arr * tests.scale
+tests.create_valsplit(0.8)
 tsampler = dataprocessing.MultiSplitSampler(tests)
 vsampler = dataprocessing.MultiSplitSampler(tests, False)
 
@@ -58,10 +55,20 @@ valloader = torch.utils.data.DataLoader(tests, batch_size=1, num_workers=1, samp
 criterion = nn.MSELoss()
 optimizer = optim.SGD(net.parameters(), lr=0.0001)
 
-try:
-    writer.add_graph(net)
-except Exception:
-    pass
-cont = embed_nets.Net_Container(net, trainloader, optimizer, criterion, True, valloader, writer)
+nets = [
+    embed_nets.Mixed_Net().double(),
+    embed_nets.Pooling_Net().double(),
+    embed_nets.Pre_net().double()
+]
 
-cont.train(100)
+data, resp = next(iter(valloader))
+
+optimizers = [optim.SGD(n.parameters(), lr=0.0001) for n in nets]
+for n in nets:
+    print(n.name)
+    writer.add_graph(n, data)
+# cont = embed_nets.Net_Container(net, trainloader, optimizer, criterion, True, valloader, writer)
+
+m_cont = embed_nets.Multi_Net_Container(nets, trainloader, optimizers, criterion, True, valloader, writer)
+
+m_cont.train(100)
